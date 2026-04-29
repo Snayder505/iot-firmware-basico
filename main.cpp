54e57f59fcaa6901e98e4667a2d5d40185b0cf27
IoT Dev <iot@example.com>
2026-04-29 18:55:01 -0500

    Update main.cpp with Hello World for ESP32

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
-﻿#include <Arduino.h>
-
-const int ledPin = 22;
+#include <Arduino.h> // Necesario para usar la función Serial
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  // Inicia la comunicación serial a 115200 baudios para poder ver los mensajes en la computadora
+  Serial.begin(115200);
+  
+  // Imprime el mensaje "Hola Mundo" una sola vez al arrancar
+  Serial.println("Hola Mundo desde ESP32!");
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
-  delay(1000);                
-  digitalWrite(ledPin, LOW);  
-  delay(1000);               
+  // También podemos imprimirlo repetidamente en el bucle si lo deseas:
+  // Serial.println("Hola Mundo continuo!");
+  // delay(2000); // Espera 2 segundos antes de volver a imprimir
 }
